2700b9927993c293302a1dd73941f09e0a58bf70
moe <maboualidev@gmail.com>
2020-07-18 21:23:21 -0400

    Adding Documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,88 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+from ezrequest import __version__
+
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'ezrequest'
+copyright = '2020, Mohammad Abouali'
+author = 'Mohammad Abouali'
+
+# The full version, including alpha/beta/rc tags
+release = __version__
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'm2r'
+]
+
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/3/', None)
+}
+
+napoleon_use_admonition_for_examples = True
+# napoleon_include_special_with_doc = True
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'private-members': True,
+    'undoc-members': True,
+    'show-inheritance': False
+}
+
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+# html_theme = 'default'
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
+
+source_suffix = '.rst'
+master_doc = 'index'
---
+++ docs/source/introduction.rst
@@ -0,0 +1,14 @@
+.. Comments
+    # Header:
+    # # 1st ====
+    # # 2nd ----
+    # # 3rd ^^^^
+    # # 4th ++++
+
+Quick Introduction
+==================
+
+What is `ezrequest`
+-----------------------
+`ezrequest` wraps around the famous `requests` package making it easier to use in certain cases. It also provides some
+utility function to parallelize sending the multiple different REST calls.
